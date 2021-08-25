145c6495a37b5948bde2d16eb8f4905985a29108
howardsukuan <56188677+howardsukuan@users.noreply.github.com>
2021-08-25 09:44:55 +0800

    Create account_info.py

---
+++ account_info.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+accountInfoDICT = {
+"email":"jacksugood@gmail.com",
+"apikey":"gQgpMy3A$uxbRgPuIPL$Rf#^Q&W$oq4",
+"USERNAME":"jacksugood@gmail.com",
+"LOKI_KEY":"PH%&cfJL3urj*NjuU##eAN_n#Cc$MfC",
+"DISCORD_TOKEN":"ODA3OTA1MTg5NDk4OTc4MzI0.YB-yVw.sx28Imo1pQDz5p-NJUx7sUE2qFY",
+"DISCORD_GUILD":"討論小圈圈"}
